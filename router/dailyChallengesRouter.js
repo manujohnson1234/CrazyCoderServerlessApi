9bea8259c2599f429887d3239e654b263d787795
manujohnson1234 <manujohnson2002@gmail.com>
2024-05-14 22:43:38 +0530

    re-corrected the daily challenge route

--- router/dailyChallengesRouter.js
+++ router/dailyChallengesRouter.js
@@ -3,3 +3,5 @@ const dailyChallengesRouter = express.Router();
 const leetcodeDailyChallenge = require('../controller/dailyChallengeController');
 
 dailyChallengesRouter.get('/leetcode', leetcodeDailyChallenge);
+
+module.exports = dailyChallengesRouter;
